f90b54639b36d146e2af9a0732c4d223bd5d8e63
BrandonMCarlson <Brandoncarlson93bc@gmail.com>
2021-12-13 08:50:40 -0800

    begin middleware to get user auth up and running for a log in.

--- Models/user.js
+++ Models/user.js
@@ -5,10 +5,32 @@ const userSchema = new mongoose.Schema({
  firstName: { type: String, required: true, minlength: 2, maxlength: 50 },
  lastName: { type: String, required: true, minlength: 2, maxlength: 50 },
  email: { type: String, required: true },
+ password : { type: String, required: true, minlength: 2, maxlength: 1024 },
  location: { type: String, required: true },
 });
 
 const User = mongoose.model('User', userSchema);
 
+const validateUser = (user) => {
+    const schema = Joi.object({
+      firstName: Joi.string().min(2).max(50).required(),
+      lastName: Joi.string().min(2).max(50).required(),
+      email: Joi.string().min(5).max(255).required().email(),
+      password: Joi.string().min(5).max(1024).required(),
+      locations: Joi.string().required(),
+    });
+    return schema.validate(user);
+  }
+  
+  const validateLogin = (req) => {
+    const schema = Joi.object({
+      email: Joi.string().min(5).max(255).required().email(),
+      password: Joi.string().min(5).max(1024).required(),
+    });
+    return schema.validate(req);
+  };
+
 
 module.exports = User;
+module.exports = validateUser;
+module.exports = validateLogin;
